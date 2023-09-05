28816b405b1558f99a15886ec9b2c6445a381c3a
Eugenniart <eugenia.toranza@gmail.com>
2023-09-05 15:20:57 -0300

    edit static_folder

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from nltk.chat.util import Chat, reflections
 import re
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 
 pairs = [
